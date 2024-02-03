d861a27474d8b67b0715a91ff123df1dbaa3407d
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-02-03 19:15:33 +0200

    Add util for generation of options, map edit and delete template to real data

---
+++ src/utils/viewDataUtil.js
@@ -0,0 +1,22 @@
+exports.difficultyLevelOptionsViewData = (difficultyLevel) => {
+    const title = [
+        'Vary Easy',
+        'Easy',
+        'Medium (Standart 3x3)',
+        'Intermediate',
+        'Expert',
+        'Hardcore'
+    ];
+
+    const options = title.map((title, index) => {
+        const value = index + 1;
+        
+        return {
+            title: `${value} - ${title}`,
+            value: value,
+            selected: Number(difficultyLevel) === value,
+        };
+    });
+
+    return options;
+};
